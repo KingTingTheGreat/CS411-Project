ec1f021eb27bc8685a31b6179421503f53821fcd
KingTingTheGreat <jting@bu.edu>
2024-04-17 15:35:22 -0400

    adjusted frontend types

--- frontend/src/components/home-content.tsx
+++ frontend/src/components/home-content.tsx
@@ -2,7 +2,7 @@ const HomeContent = () => {
 	return (
 		<div className="flex flex-col items-center">
 			<h1 className="text-4xl p-1 m-1">Welcome to our CS411 Project</h1>
-			<a href="http://localhost:6969/auth/google" className="p-1 m-1 bg-sky-200 rounded-lg">
+			<a href="http://localhost:6969/auth/google" className="px-4 py-2 m-1 font-medium text-slate-700 bg-sky-200 rounded-lg">
 				Login with Google
 			</a>
 		</div>
