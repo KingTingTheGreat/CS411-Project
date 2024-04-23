77360c418bf4dd53dcd652b4d2dad5bacfd708c0
ajp1206 <adrian@dunbarcircles.com>
2024-04-23 19:30:22 -0400

    reset mountain card file

--- frontend/src/components/mountain-card.tsx
+++ frontend/src/components/mountain-card.tsx
@@ -1,28 +1,26 @@
+
 import React, { useState } from 'react';
 import './styles/MountainCard.css';
 import MountainDetailsModal from './mountain-details-modal';
 import { Mountain } from '../types';
 
 interface MountainCardProps {
-  mountain: Mountain; // Use the updated Mountain interface
+  mountain: Mountain;
 }
 
 const MountainCard: React.FC<MountainCardProps> = ({ mountain }) => {
-  const [expanded, setExpanded] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
 
-  // Determine the color of the lifts open indicator
   const getLiftsOpenColor = (liftsOpen: number) => {
     if (liftsOpen < 3) return 'red';
     if (liftsOpen >= 3 && liftsOpen <= 8) return 'orange';
     return 'green';
   };
-        
-  // Toggles the expanded state
-  const toggleExpand = () => {
-    setExpanded(!expanded);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
-    
 
   const toggleFavorite = (event: React.MouseEvent) => {
     event.stopPropagation();
@@ -36,8 +34,8 @@ const MountainCard: React.FC<MountainCardProps> = ({ mountain }) => {
   const localIconPath = `../../weather/64x64/${timeOfDayFolder}/${iconCode}.png`;
 
   return (
-    <div>
-      <div className="mountain-card" onClick={toggleExpand}>
+    <>
+      <div className="mountain-card" onClick={toggleModal}>
         <h2 className="mountain-name">{mountain.name}</h2>
         <p className="mountain-info">{mountain.name}, {mountain.region}, {mountain.country}</p>
         <p className="mountain-lifts" style={{ color: getLiftsOpenColor(mountain.lifts.stats.open) }}>
@@ -59,13 +57,13 @@ const MountainCard: React.FC<MountainCardProps> = ({ mountain }) => {
           <polygon points="12 17.27 18.18 21 16.54 13.97 22 9.24 14.81 8.69 12 2 9.19 8.69 2 9.24 7.46 13.97 5.82 21 12 17.27"/>
         </svg>
       </div>
-      {expanded && (
-        <div className="mountain-details">
-          {/* Insert additional details here */}
-          <a href={mountain.href} target="_blank" rel="noopener noreferrer">More Info</a>
-        </div>
+      {isModalOpen && (
+        <MountainDetailsModal
+          resort={mountain}
+          onClose={toggleModal}
+        />
       )}
-    </div>
+    </>
   );
 };
 
