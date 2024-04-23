50643504aee39a1a990567ea68e331e34cc27490
ajp1206 <adrian@dunbarcircles.com>
2024-04-23 11:47:33 -0400

    commit all

--- backend/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	ID        string   `json:"id" bson:"_id"`
-	Username  string   `json:"username" bson:"username"`
-	FirstName string   `json:"firstname" bson:"firstname"`
-	LastName  string   `json:"lastname" bson:"lastname"`
-	Favorites []string `json:"favorites" bson:"favorites"`
-}
