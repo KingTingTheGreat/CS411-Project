e4ce8c4286db7c2ab7e4a4918f4ac6425ba24994
Jeffrey Ting <jting@bu.edu>
2024-04-25 13:11:03 -0400

    display mountaincards on home page

--- frontend/src/components/home-content.tsx
+++ frontend/src/components/home-content.tsx
@@ -1,70 +1,22 @@
-import ValidateToken from "../hooks/validateToken";
 import SignIn from "./sign-in";
-import MountainCard from "./mountain-short-card";
-import { MountainShort } from "../types";
+import MountainCardId from "./mountain-card-id";
+import GetProfile from "../hooks/getProfile";
 
 // placeholder data
-const exampleMountains: MountainShort[] = [
-  {
-    id: 1,
-    name: "Vail",
-    region: "CO",
-    country: "United States",
-  },
-  {
-    id: 2,
-    name: "Park City",
-    region: "UT",
-    country: "United States",
-  },
-  {
-    id: 3,
-    name: "Verbier",
-    region: "EU",
-    country: "Switzerland",
-  },
-  {
-    id: 4,
-    name: "Whistler",
-    region: "NA",
-    country: "Canada",
-  },
-  {
-    id: 5,
-    name: "Mont Tremblant",
-    region: "NA",
-    country: "Canada",
-  },
-  {
-    id: 6,
-    name: "Courcheval",
-    region: "EU",
-    country: "France",
-  },
-  {
-    id: 7,
-    name: "Killington",
-    region: "VT",
-    country: "United States",
-  },
-  {
-    id: 8,
-    name: "Niseko",
-    region: "APAC",
-    country: "Japan",
-  },
-];
+const topMountains: string[] = ["vail", "breck", "keystone"];
 
 const HomeContent = () => {
   // user is not signed in, show them the sign in button
-  if (!ValidateToken()) return <SignIn />;
+  const profile = GetProfile();
+  if (!profile) return <SignIn />;
+
   return (
     <div className="w-full flex flex-col items-center">
       <div className="w-3/5 bg-gray-500 p-2 m-2 rounded-lg">
         <h3 className="text-6xl text-center m-2">Popular mountains</h3>
         <div className="flex flex-wrap p-2">
-          {exampleMountains.map((mountain) => (
-            <MountainCard mountain={mountain} />
+          {topMountains.map((mountainId) => (
+            <MountainCardId id={mountainId} favorites={profile.favorites} />
           ))}
         </div>
       </div>
