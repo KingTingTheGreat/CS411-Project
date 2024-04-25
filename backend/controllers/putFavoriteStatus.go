e8f5de19228f8831db77bc83c2f8df4657d89553
Jeffrey Ting <jting@bu.edu>
2024-04-25 11:26:03 -0400

    removed print statements. only update in-memory version of user is no error occurs when adding to db

--- backend/controllers/putFavoriteStatus.go
+++ backend/controllers/putFavoriteStatus.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"backend/models"
 	"backend/shared"
-	"fmt"
 	"log"
 	"net/http"
 	"strings" 
@@ -15,25 +14,20 @@ import (
 )
 
 func updateUser(token string, user models.User) {
-	fmt.Println("updatd user", user)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	_, err := userCollection.UpdateOne(ctx, bson.M{"email":user.Email}, bson.M{"$set":bson.M{"favorites": user.Favorites}})
 	if err != nil {
 		log.Println(err)
+	} else {
+		shared.MyKeyStore.Set(token, user) 
 	}
-	shared.MyKeyStore.Set(token, user) 
-	fmt.Println("updated memory")
 }
 
 func PutFavoriteStatus(c echo.Context) error {
-	fmt.Println("\n\nPUTFAVORITESTATUS\n\n")
-	
 	mountainId := c.FormValue("id")
 	status := c.FormValue("status") == "true"
-
-	fmt.Println(mountainId, status)
 
 	token := c.Request().Header.Get("Authorization")
 	token = strings.TrimPrefix(token, "Bearer ")
@@ -45,14 +39,12 @@ func PutFavoriteStatus(c echo.Context) error {
 	if !ok {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"error":"Unauthorized"})
 	}
-	fmt.Println("user", user)
 
 	currentStatus := false
 	for i, v := range user.Favorites {
 		if v == mountainId {
 			if !status {
 				user.Favorites = append(user.Favorites[:i], user.Favorites[i+1:]...)
-				log.Println("REMOVED FAVORITE")
 				go updateUser(token, user)
 				return c.JSON(http.StatusOK, map[string]string{"message":"Success"})
 			}
@@ -68,6 +60,5 @@ func PutFavoriteStatus(c echo.Context) error {
 	// add mountainId to favorites
 	user.Favorites = append(user.Favorites, mountainId)
 	go updateUser(token, user)
-	log.Println("ADDED FAVORITE")
 	return c.JSON(http.StatusOK, map[string]string{"message":"Success"})
 }
